3595414b28e3cab27c21538e93ba315ae6c3236f
Aziz <80921378+aziz-zai@users.noreply.github.com>
2024-11-15 22:23:12 +0100

    react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import { LobbyJoinerComponent } from "./components/lobby-joiner"
-import { DarkModeToggle } from "./components/dark-mode-toggle"
+import { ThemeProvider } from "@/components/theme-provider";
+import { LobbyJoinerComponent } from "./components/lobby-joiner";
+import { DarkModeToggle } from "./components/dark-mode-toggle";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <DarkModeToggle />
-      <LobbyJoinerComponent />
+      <Router>
+        <DarkModeToggle />
+        <Routes>
+          <Route path="/" element={<LobbyJoinerComponent />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
